4800c92b43b80f43b132c557d5ca2d727340779b
AlexanderPro <Illarionov.pro@gmail.com>
2023-12-24 16:52:38 +0300

    Chocolatey package

---
+++ Package/Chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'smartcontextmenu'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/AlexanderPro/SmartContextMenu/releases/download/v1.0.0/SmartContextMenu_v1.0.0.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  destination   = $toolsDir
+  fileType      = 'zip'
+  url           = $url
+  softwareName  = 'SmartContextMenu*'
+  checksum      = '3dfce381ae8d3eadd574b13d21f73122e3fceb8b835b457ffa4bdc29b5449110'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
